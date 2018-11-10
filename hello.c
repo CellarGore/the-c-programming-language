9b0dd3fe46655b8c6d67e2dc8b7d8385328315b8
SalarUW <salarahmaduw@gmail.com>
2018-11-10 14:50:27 -0500

    Adding symbolic constants

--- hello.c
+++ hello.c
@@ -1,22 +1,21 @@
 #include <stdio.h>
 
+#define LOWER 0   /* lower limit of temperature table */
+#define UPPER 55  /* upper limit */
+#define STEP 2    /* Step size */
+
 /* print Fahrenheit-Celsius table
   for fahr = 0, 20, ..., 300 */
 
 main()
 {
   float fahr, celsius;
-  int lower, upper, step;
 
-  lower = 0;    /* lower limit of temperature table */
-  upper = 50;  /* upper limit */
-  step = 2;    /* step size */
-
-  celsius = lower;
+  celsius = LOWER;
   printf("%s\t%s\n", "Celsius", "Fahrenheit");
-  while (celsius <= upper) {
+  while (celsius <= UPPER) {
     fahr = (celsius * 9.0/5.0) + 32;
     printf("%3.0f\t%6.2f\n", celsius, fahr);
-    celsius = celsius + step;
+    celsius = celsius + STEP;
   }
 }
